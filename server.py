97b839ab23644351572804c66624abf43d78928c
Otávio Augusto Gonçalves Nogueira <56416850+otavioaugusto1@users.noreply.github.com>
2023-01-20 22:14:31 -0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import socket 
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+socket.bind(("localhost", 9473))
+socket.listen()
+
+conn, addr = socket.accept()
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    print("nova mensagem do host %s: %s" % (addr, data.decode()))
+conn.close()
